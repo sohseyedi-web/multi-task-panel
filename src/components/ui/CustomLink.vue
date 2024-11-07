3862b042d0ca4f15cd5da1f2d1cb4b856dfef6cc
sohseyedi <sohseyedi@gmail.com>
2024-11-07 20:28:53 +0330

    reusable navigation link component with active state styling

---
+++ src/components/ui/CustomLink.vue
@@ -0,0 +1,33 @@
+<template>
+    <li>
+      <router-link
+        :to="props.to"
+        :class="linkClass"
+      >
+        <slot />
+      </router-link>
+    </li>
+</template>
+  
+<script setup>
+  import { computed } from 'vue';
+  import { useRoute } from 'vue-router';
+  
+  const props = defineProps({
+  to: {
+    type: String,
+    required: true,
+  },
+});
+  
+  const route = useRoute();
+  
+  const linkClass = computed(() => {
+    const baseClass = 'transition-all duration-300 hover:text-white font-semibold';
+    return route.path === props.to
+      ? `${baseClass} text-zinc-100 font-semibold`
+      : `${baseClass} text-zinc-700`;
+  });
+</script>
+  
+  
