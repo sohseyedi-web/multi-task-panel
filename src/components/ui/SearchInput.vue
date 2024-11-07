439ef8a89dcb1028f94c36f329bbd29b05cbe5c9
sohseyedi <sohseyedi@gmail.com>
2024-11-07 20:29:19 +0330

    search input form component

---
+++ src/components/ui/SearchInput.vue
@@ -0,0 +1,17 @@
+<template>
+    <form class="lg:w-[300px] flex-1">
+        <input
+          type="text"
+          placeholder="Search..."
+          class="outline-none rounded-[1.1rem] bg-transparent w-full h-12 px-2 text-sm placeholder:text-gray-600 text-zinc-50 border-2 border-zinc-700"
+        />
+    </form>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style lang="">
+    
+</style>
