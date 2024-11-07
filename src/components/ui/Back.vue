8092b09cefbf97f7c1ec76beb2dfb31a544e2b82
sohseyedi <sohseyedi@gmail.com>
2024-11-07 20:28:34 +0330

    sidebar overlay component with click-to-close functionality

---
+++ src/components/ui/Back.vue
@@ -0,0 +1,15 @@
+<template>
+  <div
+    v-if="active"
+    class="lg:hidden fixed block w-full h-full top-0 left-0 bg-[rgba(0,0,0,.5)] z-10 transition-all duration-300"
+    @click="sidebarActive.toggleActive"
+  ></div>
+</template>
+<script setup>
+import { storeToRefs } from "pinia";
+import { useUiStore } from "../../store/Uistroe";
+
+const sidebarActive = useUiStore();
+
+const { active } = storeToRefs(sidebarActive);
+</script>
