d592f3605775c00a7b0dabc979ebc61606504d6f
sohseyedi <sohseyedi@gmail.com>
2024-11-09 21:29:23 +0330

    component provides a structured layout for creating a new task

---
+++ src/components/tasks/CreateTasks.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex items-center justify-center p-6">
+        <div class="flex flex-col  space-y-5 ">
+            <div class="space-y-2">
+                <h3 class="text-zinc-100 font-semibold text-3xl">Create a New Task</h3>
+                <p class="text-zinc-400 font-medium text-sm">What do you have to do today?</p>
+            </div>
+            <FormFields/>
+        </div>
+    </div>
+</template>
+<script setup>
+import FormFields from './FormFields.vue';
+
+</script>
