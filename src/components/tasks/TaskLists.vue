f397ace4cbc0f15d1035405a9e2da74281abd975
sohseyedi <sohseyedi@gmail.com>
2024-11-09 21:29:49 +0330

    provides a structured layout for displaying tasks for the day

---
+++ src/components/tasks/TaskLists.vue
@@ -0,0 +1,12 @@
+<template>
+    <section class="p-6 ">
+        <div class="space-y-2">
+            <h3 class="text-zinc-100 font-semibold text-3xl">Your task for today</h3>
+            <p class="text-zinc-400 font-medium text-sm">Let's get everything done</p>
+        </div>
+        <Table/>
+    </section>
+</template>
+<script setup>
+import Table from '../ui/Table.vue';
+</script>
