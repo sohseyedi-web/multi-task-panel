4080bc3e145c71a3a590f34e573e029b61354ce1
sohseyedi <sohseyedi@gmail.com>
2024-11-09 21:27:52 +0330

    layout creates a responsive main content area

---
+++ src/components/tasks/TaskLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <main class="grid grid-cols-2 h-[calc(100vh-5.3rem)]">
+        <section class="grid grid-rows-2 border-r border-zinc-800">
+          <CreateTasks/>
+          <div class=" border-zinc-800 border-t">ali</div>
+        </section>
+        <TaskLists/>
+      </main>
+</template>
+<script setup>
+import CreateTasks from './CreateTasks.vue';
+import TaskLists from './TaskLists.vue';
+
+</script>
