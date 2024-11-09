ce94be73ce1660e727318a2b28ffe53189ef0cb5
sohseyedi <sohseyedi@gmail.com>
2024-11-09 21:28:54 +0330

    form for adding tasks

---
+++ src/components/tasks/FormFields.vue
@@ -0,0 +1,36 @@
+<template>
+    <form class="w-full" @submit.prevent="handleSubmit">
+        <div class="flex items-center gap-x-4" >
+            <input type="text" v-model="title" placeholder="To do" class="outline-none rounded-[1.1rem] bg-transparent flex-1 h-12 px-2 text-sm placeholder:text-gray-600 text-zinc-50 border-2 border-zinc-700">
+            <SelectField @updateStatus="handleStatusUpdate"/>
+            <button class="rounded-[1.1rem] w-[120px] font-semibold bg-zinc-300 border-none h-12">Add Task</button> 
+        </div>
+        <textarea v-model="description" placeholder="Give more information about the task" class="outline-none rounded-[1.1rem] mt-3 bg-transparent w-full h-20 p-2 text-sm placeholder:text-gray-600 text-zinc-50 border-2 border-zinc-700 resize-none"></textarea>
+    </form>
+</template>
+<script setup>
+import SelectField from '../ui/SelectField.vue';
+import { ref } from 'vue';
+
+const title = ref('');
+const description = ref('');
+const status = ref('');
+
+function handleStatusUpdate(value) {
+  status.value = value;
+}
+
+async function handleSubmit() {
+  if (!title.value) {
+    alert('Please enter a task');
+    return;
+  }else{
+    console.log(title.value,description.value,status.value)
+  }
+
+
+  title.value = '';
+  description.value = '';
+  status.value = '';
+}
+</script>
