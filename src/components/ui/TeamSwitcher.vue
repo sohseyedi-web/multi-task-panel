a5cb28a8547cd4ed4bb6c5db06a3a1d55abfdc9e
sohseyedi <sohseyedi@gmail.com>
2024-11-10 10:47:24 +0330

    team switcher box

---
+++ src/components/ui/TeamSwitcher.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="rounded-[1.1rem] cursor-pointer lg:w-[200px] w-full flex items-center justify-between h-12 px-2 text-sm text-zinc-50 border-2 border-zinc-700">
+        <div class="w-8 h-8 rounded-full bg-orange-500"></div>
+        <h6 class="font-semibold">Soheil Team</h6>
+        <ChevronUpDownIcon class="size-6 text-zinc-100"/>
+    </div>
+</template>
+<script setup>
+import { ChevronUpDownIcon } from '@heroicons/vue/16/solid';
+
+
+</script>
