ae796830b2a8a05721f5170be5d56c180cba765d
sohseyedi <sohseyedi@gmail.com>
2024-11-11 10:38:00 +0330

    chart componenet

--- src/components/tasks/TaskLayout.vue
+++ src/components/tasks/TaskLayout.vue
@@ -2,12 +2,15 @@
     <main class="grid grid-cols-2 h-[calc(100vh-5.3rem)]">
         <section class="grid grid-rows-2 border-r border-zinc-800">
           <CreateTasks/>
-          <div class=" border-zinc-800 border-t">ali</div>
+          <div class=" border-zinc-800 border-t">
+            <ChartBar/>
+          </div>
         </section>
         <TaskLists/>
       </main>
 </template>
 <script setup>
+import ChartBar from './ChartBar.vue';
 import CreateTasks from './CreateTasks.vue';
 import TaskLists from './TaskLists.vue';
 
