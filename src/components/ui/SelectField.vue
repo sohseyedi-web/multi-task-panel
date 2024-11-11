81f27dd04cae72054b8639147d66937834c5cda1
sohseyedi <sohseyedi@gmail.com>
2024-11-11 10:37:30 +0330

    fixed progress value

--- src/components/ui/SelectField.vue
+++ src/components/ui/SelectField.vue
@@ -2,7 +2,7 @@
     <select @change="emitStatus($event)" class="outline-none rounded-[1.1rem] bg-[#141414] flex-1 h-12 px-2 text-sm placeholder:text-gray-600 text-zinc-50 border-2 border-zinc-700">
         <option disabled selected>Status</option>
         <option value="starting">Starting</option>
-        <option value="in progress">in Progress</option>
+        <option value="progress">Progress</option>
         <option value="finish">Finish</option>
       </select>
 </template>
