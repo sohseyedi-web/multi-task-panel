2a0b107dfdc9dc8a7b01efde7113dbf42e8e4744
sohseyedi <sohseyedi@gmail.com>
2024-11-11 18:32:26 +0330

    button component to dynamically apply styles based on active state and handle click events for value selection.

--- src/components/ui/ActiveBtn.vue
+++ src/components/ui/ActiveBtn.vue
@@ -2,7 +2,7 @@
     <button 
         @click="() => onShow(value)"
         :class="show === value ? 'bg-zinc-200 text-[#141414]' : 'border-2 bg-transparent border-zinc-200 text-zinc-200' "
-        class="font-semibold rounded-[1.1rem] transition-all duration-300 w-[120px] py-2">
+        class="font-semibold rounded-[1.1rem] transition-all duration-300 w-[115px] py-2">
         {{title}}
     </button>
 </template>
