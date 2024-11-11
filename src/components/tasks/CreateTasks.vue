52671d72690bdb8cc529a149551a10df7a267e74
sohseyedi <sohseyedi@gmail.com>
2024-11-11 18:34:16 +0330

    handle show create task with props and full width in small device

--- src/components/tasks/CreateTasks.vue
+++ src/components/tasks/CreateTasks.vue
@@ -1,6 +1,6 @@
 <template>
-    <div class="flex items-center justify-center p-6">
-        <div class="flex flex-col  space-y-5 ">
+    <div class="flex items-center justify-center p-4 lg:h-[50%] h-auto"  v-if="show === '0' || show === '1'">
+        <div class="flex flex-col  space-y-5 lg:flex-none flex-1">
             <div class="space-y-2">
                 <h3 class="text-zinc-100 font-semibold text-3xl">Create a New Task</h3>
                 <p class="text-zinc-400 font-medium text-sm">What do you have to do today?</p>
@@ -11,5 +11,10 @@
 </template>
 <script setup>
 import FormFields from './FormFields.vue';
-
+const {show} = defineProps({
+    show:{
+        type : String,
+        required : true
+    }
+})
 </script>
