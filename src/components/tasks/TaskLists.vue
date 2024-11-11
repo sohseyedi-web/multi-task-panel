0904c171ccbc1312a6e58e01b94416a1b126c57f
sohseyedi <sohseyedi@gmail.com>
2024-11-11 18:35:10 +0330

    display task section conditionally based on the show prop, render table component for task

--- src/components/tasks/TaskLists.vue
+++ src/components/tasks/TaskLists.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="p-6 ">
+    <section class="p-6 w-full lg:flex-1" v-if="show === '0' || show === '2'">
         <div class="space-y-2">
             <h3 class="text-zinc-100 font-semibold text-3xl">Your task for today</h3>
             <p class="text-zinc-400 font-medium text-sm">Let's get everything done</p>
@@ -9,4 +9,10 @@
 </template>
 <script setup>
 import Table from '../ui/Table.vue';
+const {show} = defineProps({
+    show:{
+        type : String,
+        required : true
+    }
+})
 </script>
