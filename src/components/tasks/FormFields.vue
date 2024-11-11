a375ffbc9fe5523631d0d331e579b7a7a5c6bf48
sohseyedi <sohseyedi@gmail.com>
2024-11-11 18:33:01 +0330

    add task form with dynamic buttons for mobile and desktop, handle task submission and state updates in the store.

--- src/components/tasks/FormFields.vue
+++ src/components/tasks/FormFields.vue
@@ -1,23 +1,32 @@
 <template>
     <form class="w-full" @submit.prevent="handleSubmit">
-        <div class="flex items-center gap-x-4" >
-            <input type="text" v-model="title" placeholder="To do" class="outline-none rounded-[1.1rem] bg-transparent flex-1 h-12 px-2 text-sm placeholder:text-gray-600 text-zinc-50 border-2 border-zinc-700">
+        <div class="flex items-center gap-x-4 flex-wrap" >
+            <input 
+              type="text" 
+              v-model="title"
+              placeholder="To do" 
+              class="outline-none rounded-[1.1rem] bg-transparent flex-1 h-12 px-2 text-sm placeholder:text-gray-600 text-zinc-50 border-2 border-zinc-700"
+             >
             <SelectField @updateStatus="handleStatusUpdate"/>
-            <button class="rounded-[1.1rem] w-[120px] font-semibold bg-zinc-300 border-none h-12">Add Task</button> 
+            <button class="rounded-[1.1rem] lg:block hidden w-[120px] font-semibold bg-zinc-300 border-none h-12">Add Task</button> 
         </div>
         <textarea v-model="description" placeholder="Give more information about the task" class="outline-none rounded-[1.1rem] mt-3 bg-transparent w-full h-20 p-2 text-sm placeholder:text-gray-600 text-zinc-50 border-2 border-zinc-700 resize-none"></textarea>
-    </form>
+        <button class="rounded-[1.1rem] mt-3 block lg:hidden w-full font-semibold bg-zinc-300 border-none h-12">Add Task</button> 
+      </form>
 </template>
 <script setup>
 import { useTaskStore } from '../../store/taskStore';
 import SelectField from '../ui/SelectField.vue';
 import { ref } from 'vue';
 
+
+
 const title = ref('');
 const description = ref('');
 const status = ref('');
 
 const taskStore = useTaskStore();
+
 
 function handleStatusUpdate(value) {
   status.value = value;
